afe83b073eb6ffd183b3f006c3e917ad7694a1af
jamilshaikh07 <jamilshaikh07@gmail.com>
2025-03-23 23:17:28 +0530

    dotfiles updated

---
+++ starship/.config/wezterm/wezterm.lua
@@ -0,0 +1,206 @@
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = 'Tangoesque (terminal.sexy)'
+
+config.font_size = 10
+config.window_decorations = "RESIZE"
+config.default_cursor_style = 'SteadyBar'
+
+-- Make inactive panes darker
+config.inactive_pane_hsb = {
+  saturation = 0.8, -- Adjust to your liking
+  brightness = 0.4, -- Darker inactive panes
+  hue = 1.0,
+}
+
+-- tmux
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+  {
+    mods = "LEADER",
+    key = "c",
+    action = wezterm.action.SpawnTab "CurrentPaneDomain",
+  },
+  {
+    mods = "LEADER",
+    key = "x",
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    mods = "ALT",
+    key = "[",
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    mods = "ALT",
+    key = "]",
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    mods = "ALT",
+    key = "r",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    mods = "ALT",
+    key = "e",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    mods = "ALT",
+    key = "LeftArrow",
+    action = wezterm.action.ActivatePaneDirection "Next"
+  },
+  {
+    mods = "ALT",
+    key = "DownArrow",
+    action = wezterm.action.ActivatePaneDirection "Down"
+  },
+  {
+    mods = "ALT",
+    key = "UpArrow",
+    action = wezterm.action.ActivatePaneDirection "Up"
+  },
+  {
+    mods = "ALT",
+    key = "RightArrow",
+    action = wezterm.action.ActivatePaneDirection "Prev"
+  },
+  {
+    mods = "ALT",
+    key = "h",
+    action = wezterm.action.ActivatePaneDirection "Prev"
+  },
+  {
+    mods = "ALT",
+    key = "j",
+    action = wezterm.action.ActivatePaneDirection "Down"
+  },
+  {
+    mods = "ALT",
+    key = "k",
+    action = wezterm.action.ActivatePaneDirection "Up"
+  },
+  {
+    mods = "ALT",
+    key = "l",
+    action = wezterm.action.ActivatePaneDirection "Next"
+  },
+  {
+    mods = "ALT",
+    key = "s",
+    action = wezterm.action.ActivatePaneDirection "Next"
+  },
+  {
+    mods = "ALT",
+    key = "a",
+    action = wezterm.action.ActivatePaneDirection "Prev"
+  },
+
+  {
+    mods = "LEADER",
+    key = "LeftArrow",
+    action = wezterm.action.AdjustPaneSize { "Left", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "RightArrow",
+    action = wezterm.action.AdjustPaneSize { "Right", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "DownArrow",
+    action = wezterm.action.AdjustPaneSize { "Down", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "UpArrow",
+    action = wezterm.action.AdjustPaneSize { "Up", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "w",
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = wezterm.action.AdjustPaneSize { "Left", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = wezterm.action.AdjustPaneSize { "Right", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = wezterm.action.AdjustPaneSize { "Down", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = wezterm.action.AdjustPaneSize { "Up", 5 }
+  },
+
+}
+
+for i = 1, 9 do
+  -- alt + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "ALT",
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+for i = 1, 9 do
+  -- leader + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+  local SOLID_LEFT_ARROW = ""
+  local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+  local prefix = ""
+
+  if window:leader_is_active() then
+    prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  end
+
+  if window:active_tab():tab_id() ~= 0 then
+    ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+  end -- arrow color based on if tab is first pane
+
+  window:set_left_status(wezterm.format {
+    { Background = { Color = "#b7bdf8" } },
+    { Text = prefix },
+    ARROW_FOREGROUND,
+    { Text = SOLID_LEFT_ARROW }
+  })
+end)
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ zsh/.zshrc
@@ -0,0 +1,77 @@
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="amuse"
+plugins=(argocd ansible  aws azure terraform golang git zsh-autosuggestions zsh-syntax-highlighting helm kubectl brew)
+source $ZSH/oh-my-zsh.sh
+eval "$(starship init zsh)"
+alias aa='sudo apt update'
+alias ae='sudo apt install'
+alias aq='sudo apt upgrade'
+alias dk='docker'
+alias dw='cd ~/Downloads'
+alias k='kubectl'
+complete -F __start_kubectl k
+alias kk='k get nodes,pods -o wide'
+alias ktp='kubectl top pods'
+alias ktn='kubectl top nodes'
+alias ktpa='kubectl top pod -A'
+alias kde='k delete'
+alias ked='k edit deployment'
+alias hls='helm ls'
+alias hs='helm status'
+alias ff='fzf'
+alias kc='kubectx'
+alias kn='kubens'
+alias ww='cd ~/workspace'
+alias ic='cd ~/workspace/ic'
+alias tt='cd ~/workspace/tfaz'
+alias tf='terraform'
+alias tg='terragrunt'
+alias dg='dig +short myip.opendns.com @resolver1.opendns.com'
+alias ll='ls -lcrht'
+# kubernetes
+alias kp='kubectl get pods'
+alias kshell='kubectl run --rm utils -it --image arunvelsriram/utils bash'
+alias ka='kubectl get all'
+alias kg='kubectl get'
+alias kgn='kubectl get nodes'
+alias kp='kubectl get pods'
+alias kl='kubectl logs'
+alias kf='kubectl logs -f'
+alias kd='kubectl describe'
+alias kdp='kubectl describe pod'
+alias kdd='kubectl describe deployment'
+alias kds='kubectl describe service'
+alias kdt='kubectl describe statefulset'
+autoload -U colors && colors
+alias ks='kubectl get svc'
+alias kpa='kubectl get pods -A'
+alias ke='kubectl edit'
+alias ns='nslookup -type=any'
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias gcli='gcloud'
+alias n='nvim'
+alias beam="cd ~/workspace/beamer"
+alias c='clear'
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+eval "$(ssh-agent -s)" > /dev/null 2>&1
+ssh-add ~/.ssh/jamilshaikh07-github > /dev/null 2>&1
+
+source /home/jamil-shaikh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+export PATH=$HOME/.local/bin:$PATH
+alias ub='multipass shell dazzling-grub'
+
+alias wt='wezterm cli set-tab-title'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/jamil-shaikh/google-cloud-sdk/path.zsh.inc' ]; then . '/home/jamil-shaikh/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/jamil-shaikh/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/jamil-shaikh/google-cloud-sdk/completion.zsh.inc'; fi
+
+
+export KUBE_EDITOR='code --wait'
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+

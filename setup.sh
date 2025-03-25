8c33c4a13457cbb411ed6087eaa4dcb2c6c0ffbc
jamilshaikh07 <jamilshaikh07@gmail.com>
2025-03-26 00:06:22 +0530

    dotfiles updated

--- setup.sh
+++ setup.sh
@@ -30,15 +30,6 @@ sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm -f nvim-linux-x86_64.tar.gz
 rm -rf ~/.config/nvim/.git
 
-# Install 1Password using the official repository
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-sudo apt update && sudo apt install -y 1password
-
 # Install Oh My Zsh (Non-interactive)
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     export RUNZSH="no"

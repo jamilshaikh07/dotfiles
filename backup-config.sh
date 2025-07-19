7a16cf7da0d22fba5138921d4c5631ab71b60426
jamilshaikh07 <jamilshaikh07@gmail.com>
2025-07-20 02:03:50 +0530

    chore: import laptop configs (20250720_0203)

--- backup-config.sh
+++ backup-config.sh
@@ -10,6 +10,7 @@ cp -r ~/.config/nvim      "$BACKUP/nvim"
 cp -r ~/.config/tmux "$BACKUP/tmux"
 cp ~/.zshrc               "$BACKUP/.zshrc"
 cp ~/.config/starship.toml "$BACKUP/starship.toml"
+cp ~/.vimrc             "$BACKUP/.vimrc"
 
 echo "Backed up to $BACKUP"
 
@@ -21,6 +22,7 @@ rsync -a --delete ~/dotfiles_backup_$TS/nvim/   nvim/
 rsync -a --delete ~/dotfiles_backup_$TS/tmux/   tmux/
 rsync -a --delete ~/dotfiles_backup_$TS/.zshrc .zshrc
 rsync -a --delete ~/dotfiles_backup_$TS/starship.toml starship.toml
+rsync -a --delete ~/dotfiles_backup_$TS/.vimrc .vimrc
 
 # stage, commit & push
 git add .

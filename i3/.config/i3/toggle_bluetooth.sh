2da262a8e6435bda1dddaff28d5d5b641be1218e
jamilshaikh07 <jamilshaikh07@gmail.com>
2025-09-11 14:48:48 +0530

    added bluetooth script toggling drivers for call and music in i3 config

---
+++ i3/.config/i3/toggle_bluetooth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Toggle Bluetooth active profiles function when executed shall switch between a2dp_sink and headset-head-unit-msbc
+CARD="bluez_card.84_0F_2A_39_25_B6"
+CURRENT_PROFILE=$(pactl list cards | grep -A 20 "$CARD" | grep "Active Profile:" | awk -F':' '{print $2}' | xargs)
+
+case "$CURRENT_PROFILE" in
+  "a2dp-sink")
+    pactl set-card-profile "$CARD" headset-head-unit-msbc
+    notify-send "Bluetooth Profile" "Switched to Headset Head Unit (mSBC)"
+    ;;
+  "headset-head-unit-msbc")
+    pactl set-card-profile "$CARD" a2dp-sink
+    notify-send "Bluetooth Profile" "Switched to A2DP Sink"
+    ;;
+  *)
+    notify-send "Bluetooth Profile" "Current profile is neither a2dp-sink nor headset-head-unit-msbc"
+    ;;
+esac
